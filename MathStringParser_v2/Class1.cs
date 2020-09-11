3aea8cb3b8f5262e9dc9825327c24cbc97090e3c
kolpim13 <49982100+kolpim13@users.noreply.github.com>
2020-09-11 17:27:30 +0300

    Add files via upload

---
+++ MathStringParser_v2/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathStringParser_v2
+{
+    #region //class Cell
+    public partial class Cell
+    {
+        #region //properties
+        //значение ячейки
+        public double Value { get; private set; }
+        //знак ячейки
+        public char Sign { get; private set; }
+        //является ли ячейка аргументом
+        public bool IsArgument { get; private set; }
+        //строка, отвечающая за то, как будет обозначаться аргумет(напр. "x"/"y"/"graph" и т.д.) Зарезервировано на будущее(может не понадобиться)
+        public string argumentValue { get; private set; }
+        #endregion
+        #region //constructor
+        public Cell(double value, char sign, bool isArgue = false)
+        {
+            Value = value;
+            Sign = sign;
+            IsArgument = isArgue;
+
+            //////////////////////
+            argumentValue = "hz :)";
+        }
+        #endregion
+        #region //functions
+        //Проверка есть ли данный знак в словаре знаков и соответсвующих им функций
+        public static bool IsSign(char c)
+        {
+            if (Actions.ContainsKey(c))
+                return true;
+            return false;
+        }
+
+        //обьеденить (согласно знаку левой) 2 ячейки (правую и левую) и сохранить результат в левой, а знак оставить правой. Вернуть bool, которая укажетбылали произведена операция
+        public static bool Merge(Cell leftCell, Cell rightCell)
+        {
+            if (IsSign(leftCell.Sign))
+            {
+                leftCell.Value = Actions[leftCell.Sign](leftCell.Value, rightCell.Value);
+                leftCell.Sign = rightCell.Sign;
+                return true;
+            }
+            return false;
+        }
+        #endregion
+    }
+    #endregion
+
+    #region //class Parser
+    public partial class Parser
+    {
+        private List<Cell> listToMerge;
+
+        public Parser()
+        {
+            listToMerge = new List<Cell>();
+        }
+
+        //основная функция - раскладывает строку нужного формата на "Cell" и считает выражение
+        public double SplitAndCalc(string data, ref int index, double arg = 1, int listIndex = 0)
+        {
+            Cell a = new Cell(10, '+');
+            Cell b = new Cell(20, '+');
+            Cell.Merge(a, b);
+            return a.Value;
+        }
+
+
+    }
+    #endregion
+
+    #region //class StringProcessing
+
+    #endregion
+}
---
+++ MathStringParser_v2/Extension.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathStringParser_v2
+{
+    //Перечисление, которая отображает как закончилась та или иная операция
+    public enum Rezult
+    {
+        FAILED = 0,
+        SUCCESS = 1
+    };
+
+    #region //Расширения для класса Cell
+    public partial class Cell
+    {
+        //словарь старшинства операций
+        private static Dictionary<char, int> ActionPrecedence = new Dictionary<char, int>
+        {
+            { '+', 1 },
+        };
+        //словарь простых математических операций между 2 "Cell"
+        private static Dictionary<char, Func<double, double, double>> Actions = new Dictionary<char, System.Func<double, double, double>>
+        {
+            { '+', new Func<double, double, double>(AddCells) }
+        };
+
+        //Символы, что уже используются в программе.
+        private static List<char> ReservedSigns = new List<char>
+        {
+            { '+' },
+        };
+
+        //добавление в словарь новых пар (знак - метод) и в словарь старшинства (знак - старшиснтво операции). Возвращает значение успешно прошла оперцаия или нет
+        public static Rezult AddAction(char sign, Func<double, double, double> action, int precedence = 1)
+        {
+            if (ReservedSigns.Contains(sign))
+                return Rezult.FAILED;
+
+            Actions.Add(sign, action);
+            ActionPrecedence.Add(sign, precedence);
+            ReservedSigns.Add(sign);
+            return Rezult.SUCCESS;
+        }
+
+        //Оперции доступные для действия с клетками по умолчанию
+        private static double AddCells(double left, double right)
+        {
+            return left + right;
+        }
+        #endregion
+    }
+}
